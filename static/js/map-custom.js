abba3f6c0c7e6b205a568b22a04484f3116d1904
evanmetsky <emetsky@umich.edu>
2017-12-04 17:06:37 -0500

    credit where credit is due

--- static/js/map-custom.js
+++ static/js/map-custom.js
@@ -60,6 +60,8 @@ $(document).ready(function() {
 
 
 	});
+
+/*adapted from: https://www.w3schools.com/howto/howto_js_filter_lists.asp */
 	function filter() {
 			    var input, filter, ul, li, a, i;
 			    input = document.getElementById("search");
